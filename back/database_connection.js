8f30c0cc094015d36592a8207590136ddcb3f170
JuanDT24 <tiradodj@uninorte.edu.co>
2025-05-29 21:50:06 -0500

    Returning cookie on login

--- back/database_connection.js
+++ back/database_connection.js
@@ -1,47 +1,10 @@
 // Dependencies
-<<<<<<< HEAD
-const { MongoClient, ServerApiVersion } = require("mongodb");
-require("dotenv").config();
-=======
 const {MongoClient, ServerApiVersion} = require('mongodb');    
 require('dotenv').config(); 
->>>>>>> d59c31104197bdbf4f8e66768a8e1e99c936e040
 
 const dbkey = process.env.DATABASE_KEY;
 
 class Connection {
-<<<<<<< HEAD
-  static #client;
-  static #db;
-  static #options = {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  };
-
-  static async connect() {
-    if (Connection.#db) return Connection.#db;
-    try {
-      Connection.#client = new MongoClient(dbkey, Connection.#options);
-      await Connection.#client.connect();
-      Connection.#db = Connection.#client.db("cnapp");
-      return Connection.#db;
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  static async close() {
-    if (Connection.#client) {
-      await Connection.#client.close();
-      Connection.#db = null;
-    }
-  }
-}
-
-module.exports = Connection;
-=======
     static #client;
     static #db;
     static #options = {
@@ -76,4 +39,3 @@ module.exports = Connection;
 
 module.exports = Connection;
 
->>>>>>> d59c31104197bdbf4f8e66768a8e1e99c936e040
