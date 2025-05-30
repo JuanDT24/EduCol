b91078f62b1296ee71ff0f89b72933d86f879711
JuanDT24 <tiradodj@uninorte.edu.co>
2025-05-30 01:35:27 -0500

    Added teacher and student endpoints

--- back/controllers/studentController.js
+++ back/controllers/studentController.js
@@ -1,6 +1,43 @@
 //Imports with dependencies
 const Connection = require('../database_connection.js');
+const { ObjectId } = require('mongodb');
 class studentController {
+    static async getDB() {
+    if (!this.db) {
+      this.db = await Connection.connect();
+    }
+    return this.db;
+  }
+  static async getStudentById(studentId) {
+    const db = await this.getDB();
+    const studentIdObject = new ObjectId(studentId);
+    const student = await db.collection('users').findOne({ _id: studentIdObject });
+    // Check if student exists
+     if (!student) {
+      throw new Error('Estudiante no encontrado');
+    }
+    // Check if the user is a teacher
+    if(student.isTeacher) {
+      throw new Error('Esto es un profesor, no un estudiante');
+    }
+   
+    return student;
+  }
+  static async getStudentByUsername(studentName) {
+    const db = await this.getDB();
+    const student = await db.collection('users').findOne({ username: studentName});
+    // Check if student exists
+     if (!student) {
+      throw new Error('Estudiante no encontrado');
+    }
+    // Check if the user is a teacher
+    if(student.isTeacher) {
+      throw new Error('Esto es un profesor, no un estudiante');
+    }
+   
+    return student;
+  }
+
 }
 
 module.exports = studentController
